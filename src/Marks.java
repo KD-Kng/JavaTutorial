cf5a63053d55c93ef43b6bec5ad5bd080c606c94
KD Kang <kdkang@kds-macbook-air.home>
2024-01-24 22:01:29 -0500

    Practice Que

---
+++ src/Marks.java
@@ -0,0 +1,12 @@
+/*Question 2) Take marks for 6 subjects and then find the average of their marks. Then assign a grade to that student based on the average mark from the table below :
+        90+ = A+
+        85-90 = A
+        79-85 = A-
+        71-79 = B+
+        60-71 = B-
+        51-60 = B
+        Below = Fail
+        Test this for 5 students each having 6 subjects. Print the average and grade of each student. At last find and print the student who got the third highest grade for average marks.
+*/
+public class Marks {
+}
---
+++ src/Number.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/* Question 1) Check whether a number is single digit, double digit or three digit number
+and then print if it's single, double or three digit number.
+If the number has more than 3 digits then print "invalid number".
+If the number is negative, then also print "Invalid number".
+You can create a variable and then assign any value of your choice and then test on that number.
+*/
+public class Number {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+
+        int counter = 0;
+//        x = 10//10
+        if (x<0)
+        {
+            System.out.println("Number is Invalid");
+        } else if (x==0) {
+            System.out.println("Number is 1 digit long");
+        } else {
+            while (x > 0) {
+                counter++;
+                x = x / 10;
+            }
+            System.out.println("x is " + counter + " digit long");
+        }
+
+    }
+
+}
