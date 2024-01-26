188c43f274ca85219f410f198f52c78355a00491
KD Kang <kdkang@kds-macbook-air.home>
2024-01-25 19:09:01 -0500

    Practice Que 2

--- src/Marks.java
+++ src/Marks.java
@@ -1,4 +1,7 @@
-/*Question 2) Take marks for 6 subjects and then find the average of their marks. Then assign a grade to that student based on the average mark from the table below :
+import java.util.Scanner;
+
+/*Question 2) Take marks for 6 subjects and then find the average of their marks.
+Then assign a grade to that student based on the average mark from the table below :
         90+ = A+
         85-90 = A
         79-85 = A-
@@ -6,7 +9,38 @@
         60-71 = B-
         51-60 = B
         Below = Fail
-        Test this for 5 students each having 6 subjects. Print the average and grade of each student. At last find and print the student who got the third highest grade for average marks.
+        Test this for 5 students each having 6 subjects.
+        Print the average and grade of each student.
+        At last find and print the student who got the third highest grade for average marks.
 */
 public class Marks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            int maths = sc.nextInt();
+            int sst = sc.nextInt();
+            int hindi = sc.nextInt();
+            int chem = sc.nextInt();
+            int phy = sc.nextInt();
+            int bio = sc.nextInt();
+
+            int avg = (maths + sst + hindi + chem + phy + bio) / 6;
+            System.out.println("Average marks" + avg);
+
+            if (avg > 90) {
+                System.out.println("Grade is A+");
+            } else if (avg < 90 && avg > 85) {
+                System.out.println("Grade is A");
+            } else if (avg < 85 && avg > 79) {
+                System.out.println("Grade is A-");
+            } else if (avg < 79 && avg > 71) {
+                System.out.println("Grade is B+");
+            } else if (avg < 71 && avg > 60) {
+                System.out.println("Grade is B");
+            } else if (avg < 60 && avg > 51) {
+                System.out.println("Grade is B-");
+            } else {
+                System.out.println("Fail");
+        }
+    }
 }
