25e620af9faf108a626793748532db6c0f6cb503
KD Kang <kdkang@kds-macbook-air.home>
2024-01-31 16:00:47 -0500

    Practice Que 3

---
+++ src/Loop.java
@@ -0,0 +1,23 @@
+/*Question 1) create the following pattern using the for loop. pattern is:
+        *    1
+        **   21
+        ***
+        ****
+        ***** */
+public class Loop {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<5; i++)
+        {
+            System.out.println("*");
+            for (int j=0; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+
+
+        }
+
+    }
+}
