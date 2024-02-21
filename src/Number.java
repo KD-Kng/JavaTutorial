4f8ac64760bc3258f5ece1e6aeafeb566101f403
Rohit Bhardwaj <rohit@Rohits-MacBook-Pro-2.local>
2024-02-20 22:10:31 -0400

    added exercise file

--- src/Number.java
+++ src/Number.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 and then print if it's single, double or three digit number.
 If the number has more than 3 digits then print "invalid number".
 If the number is negative, then also print "Invalid number".
-You can create a variable and then assign any value of your choice and then test on that number.
+You can create a variable and then assign any value of your choice and then ToLearn on that number.
 */
 public class Number {
 
